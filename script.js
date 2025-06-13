cc8c5a2d7ce48f6dd55742202c658f0144bf9229
manvesh12 <168457140+manvesh12@users.noreply.github.com>
2025-06-13 22:19:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function append(value) {
+  document.getElementById("display").value += value;
+}
+
+function clearDisplay() {
+  document.getElementById("display").value = "";
+}
+
+function calculate() {
+  try {
+    const result = eval(document.getElementById("display").value);
+    document.getElementById("display").value = result;
+  } catch {
+    document.getElementById("display").value = "Error";
+  }
+}
+
+function toggleTheme() {
+  document.body.classList.toggle("dark");
+}
